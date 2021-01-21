7732293878ee26c33a580d1b1ceb1ac881790020
BhavyaaKohli <74053632+BhavyaaKohli@users.noreply.github.com>
2021-01-21 09:29:12 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,34 @@
+class Ball {
+
+    constructor(x,y,r){
+     var options={
+
+     
+       density :5,
+       frictionAir :0.005
+ }
+        this.r =r
+ 
+        this.body = Bodies.circle(x , y , r)
+        World.add(world ,this.body)
+ 
+ 
+    }
+ 
+ 
+ display(){
+ 
+   var pos= this.body.position
+   var angle = this.body.angle
+     push()
+     translate(pos.x , pos.y)
+     rotate(angle)
+     fill("blue")
+     ellipseMode(RADIUS)
+     ellipse(0,0,this.r,this.r)
+     pop()
+ 
+ }
+ 
+ 
+}
